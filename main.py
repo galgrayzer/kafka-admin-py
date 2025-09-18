ba33f0ebc98e267bef74acaba68cc6f5d6699518
galgrayzer <galgrayzer@gmail.com>
2025-09-18 23:24:18 +0300

    Created FastAPI app

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import uvicorn
 from src.factory import AppFactory
 from src.settings import fast_settings
 from src.settings import app_settings
+from src.logger import logger
 
 fast_app: FastAPI = AppFactory.create_app()
 
 if __name__ == "__main__":
+    logger.info(
+        f"Starting {fast_app.title} on {fast_settings.host}:{fast_settings.port}"
+    )
     uvicorn.run(
-        fast_app,
+        "main:fast_app",
         host=fast_settings.host,
         port=fast_settings.port,
         reload=fast_settings.reload,
         workers=app_settings.workers,
     )
-    
--- src/settings/_fast_settings.py
+++ src/settings/_fast_settings.py
@@ -1,19 +1,25 @@
 from pydantic import Field, PositiveInt
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from os import getenv
 
 from src.models.enums import AppEnvironment
-from . import app_settings
 
 
 class FastSettings(BaseSettings):
     host: str = Field(default="0.0.0.0", alias="FAST_HOST")
-    port: PositiveInt = Field(default=8000, alias="FAST_PORT")
+    port: PositiveInt = Field(default=3000, alias="FAST_PORT")
     reload: bool = Field(
-        default=(app_settings.environment is AppEnvironment.DEVELOPMENT),
+        default=(
+            getenv("APP_ENVIRONMENT", AppEnvironment.DEVELOPMENT)
+            is AppEnvironment.DEVELOPMENT
+        ),
         alias="FAST_RELOAD",
     )
-    title: str = Field(default="My API", alias="FAST_TITLE")
-    description: str = Field(default="This is a sample API", alias="FAST_DESCRIPTION")
+    title: str = Field(default="Kafka Admin PY", alias="FAST_TITLE")
+    description: str = Field(
+        default="A REST API that does Kafka operations on clusters",
+        alias="FAST_DESCRIPTION",
+    )
     version: str = Field(default="1.0.0", alias="FAST_VERSION")
 
     model_config = SettingsConfigDict(env_file=".env")
