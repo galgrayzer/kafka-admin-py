cadacf84c79b09fc57ea8d68d16dc8279c9941a5
galgrayzer <galgrayzer@gmail.com>
2025-09-20 22:53:50 +0300

    Moved middlewares to v1 level

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+
+from app.api.v1.middlewares import (
+    extract_bootstrap_servers,
+    extract_kafka_credentials,
+    create_kafka_client,
+)
+
+from app.api.v1.swagger_docs import (
+    docs_bootstrap_servers,
+    docs_kafka_password,
+    docs_kafka_username,
+)
+
 from .endpoints import topic_router
 
 v1_router = APIRouter(prefix="/v1")
-v1_router.include_router(topic_router)
+v1_router.include_router(
+    topic_router,
+    dependencies=[
+        Depends(docs_bootstrap_servers),
+        Depends(docs_kafka_username),
+        Depends(docs_kafka_password),
+        Depends(extract_bootstrap_servers),
+        Depends(extract_kafka_credentials),
+        Depends(create_kafka_client),
+    ],
+)
 
 __all__ = ["v1_router"]
--- app/api/v1/endpoints/topic/topic_route.py
+++ app/api/v1/endpoints/topic/topic_route.py
@@ -1,27 +1,3 @@
 from fastapi import APIRouter, Depends
 
-
-from app.api.v1.middlewares import (
-    extract_bootstrap_servers,
-    extract_kafka_credentials,
-    create_kafka_client,
-)
-
-from app.api.v1.swagger_docs import (
-    docs_bootstrap_servers,
-    docs_kafka_password,
-    docs_kafka_username,
-)
-
-topic_router = APIRouter(
-    prefix="/topic",
-    tags=["topic"],
-    dependencies=[
-        Depends(docs_bootstrap_servers),
-        Depends(docs_kafka_username),
-        Depends(docs_kafka_password),
-        Depends(extract_bootstrap_servers),
-        Depends(extract_kafka_credentials),
-        Depends(create_kafka_client),
-    ],
-)
+topic_router = APIRouter(prefix="/topic", tags=["topic"])
